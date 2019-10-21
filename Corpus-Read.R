adc192f7fe18342427e9b79799a8d57968babbe6
Jameson Watts <jameson.watts@gmail.com>
2019-10-21 14:08:52 -0700

    Finally, a good read

--- Corpus-Read.R
+++ Corpus-Read.R
@@ -5,45 +5,14 @@ library(tm)
 library(readxl)
 library(SnowballC)
 
-# Read the text from pdf's with pdftools package
-dir <- "../Corpus/"
-ids <- str_remove(list.files(dir, pattern = "*.pdf", recursive = FALSE),pattern = ".pdf")
-files <- paste0(dir, list.files(dir, pattern = "*.pdf", recursive = FALSE)) #adds directory to front
-pdfs <- map(files, pdftools::pdf_text)
-
-## Get rid of last three paragraphs (references etc.)
-#TODO: figure out better way of determining references
-trim = 2 # number of paragraphs to trim from the end
-for(i in 1:length(pdfs)){
-  if(length(sample)>trim){
-    pdfs[[i]] <- pdfs[[i]][1:(length(pdfs[[i]])-trim)]
-  }
-  pdfs[[i]] <- paste(sample[[i]],sep = " ", collapse = "")
-}
-
-# Create token db
-data(stop_words)
-engagement <- tibble(document=ids, text=pdfs) %>% 
-  unnest_tokens(word, text) %>% 
-  anti_join(stop_words) %>% # get rid of stop words
-  mutate(term = wordStem(word)) %>%
-  count(document, term) %>%
-  filter(!str_detect(string = term, pattern = "[0-9+]")) %>%  # get rid of numbers
-  filter(str_detect(string = term, pattern = "[a-z+]")) %>%  # get rid of numbers
-  filter(str_length(term)>3) %>%  # get rid of strings shorter than 3 characters
-  right_join(y=read_excel(str_c(dir,"Database.xlsx")),by=c("document"="ID NUMBER")) %>% 
-  rename("Author"="Author, editor or organization") %>% 
-  mutate(year=as.numeric(`Year derived`)) %>% 
-  filter(year < 2019) %>% 
-  rename_all(tolower) 
-
-names(engagement)<-str_replace_all(names(engagement), c(" " = "_" , "," = "" ))
-
-engagement <- engagement %>% 
-  group_by(term) %>% 
-  summarize(total=sum(n)) %>% 
-  right_join(engagement) %>% 
-  filter(total > 10) %>% # only include terms that show up more than 10 times overall
+# Read in the excel database and clean it up
+dbx <- as_tibble(read_excel(str_c(dir,"Database.xlsx")))
+names(dbx)<-str_replace_all(names(dbx), c(" " = "_" , "," = "" ))
+dbx <- dbx %>% 
+  rename_all(tolower) %>% 
+  rename("author"="author_editor_or_organization") %>% 
+  rename("document"="id_number") %>% 
+  mutate(year=as.numeric(year_derived)) %>% 
   mutate(periodical_short=str_extract(periodical,"^[^\\(]+")) %>% # get rid of periodical in parentheses
   mutate(periodical_short=str_replace_all(periodical_short,"[Jj]ournal","J.")) %>% 
   mutate(periodical_short=str_replace_all(periodical_short,"[Ii]nternational","Int.")) %>% 
@@ -68,6 +37,61 @@ engagement <- engagement %>%
       research_field=="Public Management Research" ~ "Public"
     )
   ) %>% 
+  write_rds(path = "articles.rds")
+
+# Read the text from pdf's with pdftools package
+dir <- "../Corpus/"
+ids <- str_remove(list.files(dir, pattern = "*.pdf", recursive = FALSE),pattern = ".pdf")
+files <- paste0(dir, list.files(dir, pattern = "*.pdf", recursive = FALSE)) #adds directory to front
+pdfs <- map(files, pdftools::pdf_text)
+
+#TODO: figure out better way of determining references
+trim = 2 # number of paragraphs to trim from the end
+for(i in 1:length(pdfs)){
+  if(length(pdfs[[i]])>trim){
+    pdfs[[i]] <- pdfs[[i]][1:(length(pdfs[[i]])-trim)]
+  }
+  pdfs[[i]] <- paste(pdfs[[i]],sep = " ", collapse = "")
+}
+
+# Create token db
+data(stop_words)
+engagement <- tibble(document=ids, text=pdfs) %>% 
+  mutate(text=as.character(text)) %>% 
+  mutate(text = str_replace_all(text,"[\n\t\r\v\f]"," ")) %>% 
+  mutate(text = str_replace_all(text,"\\s+"," ")) %>% 
+  unnest_tokens(word, text) %>% 
+  anti_join(stop_words) %>% # get rid of stop words
+  filter(!str_detect(string = word, pattern = "[0-9+]")) %>%  # get rid of numbers
+  filter(str_detect(string = word, pattern = "[a-z+]")) %>%  # get rid weird non alphas
+  filter(str_length(word)>3) %>%  # get rid of strings shorter than 3 characters
+  mutate(term = wordStem(word)) %>%
+  count(document, term) %>%
   rename("count"="n") %>% 
-  write_rds(path = "engagement.rds")
+  group_by(term) %>% 
+  mutate(total=sum(count)) %>% 
+  ungroup() %>% 
+  filter(total > 10) %>% # only include terms that show up more than 10 times overall
+  write_rds(path = "terms.rds")
+
+# Create bigram db
+engagement_bi <- tibble(document=ids, text=pdfs) %>% 
+  mutate(text=as.character(text)) %>% 
+  mutate(text = str_replace_all(text,"[\n\t\r\v\f]"," ")) %>% 
+  mutate(text = str_replace_all(text,"\\s+"," ")) %>% 
+  unnest_tokens(bigram, text, token = "ngrams", n=2) %>%
+  filter(!str_detect(string = bigram, pattern = "[0-9+]")) %>%  # get rid of numbers
+  filter(str_detect(string = bigram, pattern = "[a-z+]")) %>%  # get rid weird non alphas
+  count(document, bigram) %>% 
+  rename("count"="n") %>% 
+  group_by(bigram) %>% 
+  mutate(total=sum(count)) %>% 
+  ungroup() %>% 
+  filter(total > 5) %>% # only include bigrams that show up more than 10 times overall
+  separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>% 
+  mutate(word1 = wordStem(word1)) %>%
+  mutate(word2 = wordStem(word2)) %>% 
+  write_rds(path = "bigrams.rds")
 
